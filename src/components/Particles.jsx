79fc0b8f2557ad59934f56b1b0b8bdbbafffee15
Johncent Ventura <johncent.developer@gmail.com>
2024-06-06 09:28:46 +0800

    Setup npm run build

--- src/components/Particles.jsx
+++ src/components/Particles.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useMemo, useState } from "react";
 // import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
