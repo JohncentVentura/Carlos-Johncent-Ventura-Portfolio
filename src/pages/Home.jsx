4bd2e7e2feec6cc1e749851aa44b84e8d0910805
Johncent Ventura <johncent.developer@gmail.com>
2024-06-08 18:16:01 +0800

    GSAP 02: Designing Home Page * Fixed Bug: Not scrolling in mobile * Refractord GSAP Functions * Testing Home Page Animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,7 @@ import { rule } from "postcss";
 
 const Home = ({ ...props }) => {
   gsap.registerPlugin(ScrollTrigger);
-
-  useEffect(() => {
-    UseEffects();
-  });
+  useEffect(() => UseEffects());
 
   return (
     <>
@@ -28,17 +25,120 @@ const Home = ({ ...props }) => {
         </div>
         */}
 
-        <div className="div1 pt-[40vh] pb-[10vh] text-center text-5xl">
+        <div className="div1 pt-[40vh] pb-[25vh] text-center text-5xl">
           Hello!
         </div>
 
-        <div className="div2 pt-[30vh] text-center text-5xl">
-          I'm Carlos Johncent Ventura
+        <div className="div2 pt-[20vh] text-center text-5xl">
+          I'm Carlos Johncent Ventura,
         </div>
 
-        <div className="clip-parent pt-[5vh] text-center text-5xl flex justify-center items-center">
-          <div className="clip-path w-64 h-64 bg-primaryClr flex justify-center items-center">
-            <img src={ImagePaths.hero2} alt={ImagePaths.hero2} className="w-full h-full object-fill"/>
+        <LgRhombus parentClass={"pt-[5vh]"} clipPathClass={"clipShow"}>
+          <img
+            src={ImagePaths.hero}
+            alt={ImagePaths.hero}
+            className="w-full h-full object-fill"
+          />
+        </LgRhombus>
+
+        <div className="div3 pt-[10vh] text-center text-5xl">
+          Just another fool chasing the dream of
+        </div>
+
+        <div className="flex justify-evenly items-center">
+          <LgRhombus
+            parentClass={"mt-[20vh]"}
+            clipPathClass={"clipLeftToRight"}
+          >
+            <img
+              src={ImagePaths.hero}
+              alt={ImagePaths.hero}
+              className="w-full h-full object-fill"
+            />
+          </LgRhombus>
+
+          <div className="div4 pt-[20vh] text-center text-5xl">
+            Front-End Development
+          </div>
+        </div>
+
+        <div className="flex justify-evenly items-center">
+          <div className="div5 pt-[20vh] text-center text-5xl">
+            Back-End Development
+          </div>
+
+          <LgRhombus
+            parentClass={"mt-[20vh]"}
+            clipPathClass={"clipRightToLeft"}
+          >
+            <img
+              src={ImagePaths.hero}
+              alt={ImagePaths.hero}
+              className="w-full h-full object-fill"
+            />
+          </LgRhombus>
+        </div>
+
+        <div className="flex justify-evenly items-center">
+          <LgRhombus
+            parentClass={"mt-[20vh]"}
+            clipPathClass={"clipLeftToRight2"}
+          >
+            <img
+              src={ImagePaths.hero}
+              alt={ImagePaths.hero}
+              className="w-full h-full object-fill"
+            />
+          </LgRhombus>
+
+          <div className="div6 pt-[20vh] text-center text-5xl">
+            Game Development
+          </div>
+        </div>
+
+        <div className="flex justify-evenly items-center">
+          <div className="div7 pt-[20vh] text-center text-5xl">
+            Graphics Design & Pixel Art
+          </div>
+
+          <LgRhombus
+            parentClass={"mt-[20vh]"}
+            clipPathClass={"clipRightToLeft2"}
+          >
+            <img
+              src={ImagePaths.hero}
+              alt={ImagePaths.hero}
+              className="w-full h-full object-fill"
+            />
+          </LgRhombus>
+        </div>
+
+        <div className="div8 pt-[30vh] text-center text-5xl">
+          And still learning more, but for now...
+        </div>
+
+        <div className="mt-[50vh] h-[105vh] flex justify-center items-center">
+          <LgRhombus parentClass={"absolute top-0 "} clipPathClass={"clipShow2"}>
+            <img
+              src={ImagePaths.hero}
+              alt={ImagePaths.hero}
+              className="w-full h-full object-fill"
+            />
+          </LgRhombus>
+
+          <div className="div8 absolute top-0 pt-[25vh] text-center text-6xl">
+            Welcome to my portfolio
+          </div>
+
+          <div className="div8 absolute top-0 pt-[35vh] text-center text-2xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae rerum fuga mollitia laborum placeat sequi.
+          </div>
+
+          <div className="absolute top-0 w-[50vw] flex justify-evenly items-center">
+            <SmRhombus parentClass={"mt-[15vh] pt-[30vh]"} clipPathClass={"clipShow3"}></SmRhombus>
+            <SmRhombus parentClass={"mt-[15vh] pt-[30vh]"} clipPathClass={"clipShow3"}></SmRhombus>
+            <SmRhombus parentClass={"mt-[15vh] pt-[30vh]"} clipPathClass={"clipShow3"}></SmRhombus>
+            <SmRhombus parentClass={"mt-[15vh] pt-[30vh]"} clipPathClass={"clipShow3"}></SmRhombus>
           </div>
         </div>
       </section>
@@ -46,26 +146,78 @@ const Home = ({ ...props }) => {
   );
 };
 
-function UseEffects() {
-  document.querySelectorAll(".div1").forEach((char, i) => Div1(char));
-  document.querySelectorAll(".div2").forEach((char, i) => Div2(char));
-  document
-    .querySelectorAll(".clip-parent")
-    .forEach((element, i) => ClipParent(element));
-}
+const SmRhombus = ({ parentClass, clipPathClass, children }) => {
+  return (
+    <>
+      <div className={`${parentClass} flex justify-center items-center`}>
+        <div
+          className={`${clipPathClass} clip-path w-36 h-36 bg-primaryClr flex justify-center items-center`}
+        >
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
 
-function Div1(char) {
-  gsap
-    .timeline({
+const LgRhombus = ({ parentClass, clipPathClass, children }) => {
+  return (
+    <>
+      <div className={`${parentClass} flex justify-center items-center`}>
+        <div
+          className={`${clipPathClass} clip-path w-80 h-80 bg-primaryClr flex justify-center items-center`}
+        >
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
+
+function UseEffects() {
+  function CreateGsapTimeline({
+    trigger,
+    start,
+    end,
+    scrub,
+    markers,
+    toggleActions,
+  }) {
+    if (scrub === undefined) scrub = true;
+    return gsap.timeline({
       scrollTrigger: {
-        trigger: char,
-        start: "top center",
-        end: "bottom center",
-        scrub: false,
-        //markers: true,
+        trigger: trigger,
+        start: start || "top 65%",
+        end: end || "bottom 65%",
+        scrub: scrub,
+        markers: markers || false,
+        toggleActions: toggleActions || "play play reverse reverse",
       },
-    })
-    .fromTo(
+    });
+  }
+
+  function SetEffect(className, callback) {
+    return document.querySelectorAll(className).forEach(callback);
+  }
+
+  SetEffect(".div1", (elem) => AutoStaggerText(elem));
+  SetEffect(".div2", (elem) => StaggerText(elem));
+  SetEffect(".clipShow", (elem) => ShapeShow(elem));
+  SetEffect(".div3", (elem) => FadeInText(elem));
+  SetEffect(".clipLeftToRight", (elem) => ShapeLeftToRight(elem));
+  SetEffect(".div4", (elem) => StaggerTextRightToLeft(elem));
+  SetEffect(".div5", (elem) => StaggerTextLeftToRight(elem));
+  SetEffect(".clipRightToLeft", (elem) => ShapeRightToLeft(elem));
+  SetEffect(".clipLeftToRight2", (elem) => ShapeLeftToRight(elem));
+  SetEffect(".div6", (elem) => StaggerTextRightToLeft(elem));
+  SetEffect(".div7", (elem) => StaggerTextLeftToRight(elem));
+  SetEffect(".clipRightToLeft2", (elem) => ShapeRightToLeft(elem));
+  SetEffect(".div8", (elem) => StaggerTextFromY(elem));
+  SetEffect(".clipShow2", (elem) => ShapeShow(elem));
+  SetEffect(".clipShow3", (elem) => ShapeShowCenter(elem));
+
+  function AutoStaggerText(char) {
+    CreateGsapTimeline({ trigger: char, scrub: false, markers: false }).fromTo(
       new SplitType(char, { types: "chars" }).chars,
       {
         opacity: 0,
@@ -76,60 +228,147 @@ function Div1(char) {
         stagger: 0.3,
       }
     );
-}
+  }
 
-function Div2(char) {
-  gsap
-    .timeline({
-      scrollTrigger: {
-        trigger: char,
-        start: "top center",
-        end: "bottom center",
-        scrub: true,
-        markers: true,
-      },
-    })
-    .fromTo(
+  function StaggerText(char) {
+    CreateGsapTimeline({ trigger: char, markers: true }).fromTo(
       new SplitType(char, { types: "chars" }).chars,
       {
         opacity: 0,
       },
       {
-        //duration: 1,
         opacity: 1,
         stagger: 0.5,
       }
     );
-}
+  }
 
-function ClipParent(element) {
-  gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: element,
-      start: "top center",
-      end: "bottom center",
-      scrub: true,
-      markers: true,
-    },
-  })
-  .fromTo(
-    ".clip-path",
-    {
-      opacity: 0,
-      rotate: 360,
-      scale: 0,
-    },
-    {
-      //duration: 1,
-      opacity: 1,
-      rotate: 0,
-      scale: 1,
-      //stagger: 0.5,
-    }
-  );
+  function ShapeShow(elem) {
+    CreateGsapTimeline({ trigger: elem.parentElement, markers: true }).fromTo(
+      `.${elem.className.split(" ")[0]}`,
+      {
+        opacity: 0,
+        rotate: 360,
+        scale: 0,
+      },
+      {
+        opacity: 1,
+        rotate: 0,
+        scale: 1,
+        zIndex: -0,
+      }
+    );
+  }
 
-  console.log(`.${element.className.split(' ')[0]}`)
+  function FadeInText(char) {
+    CreateGsapTimeline({ trigger: char, markers: false }).fromTo(
+      new SplitType(char, { types: "chars" }).chars,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+      }
+    );
+  }
+
+  function ShapeLeftToRight(elem) {
+    CreateGsapTimeline({ trigger: elem.parentElement, markers: false }).fromTo(
+      `.${elem.className.split(" ")[0]}`,
+      {
+        opacity: 0,
+        rotate: -360,
+        scale: 0,
+        x: -800,
+      },
+      {
+        opacity: 1,
+        rotate: 0,
+        scale: 1,
+        x: 5,
+      }
+    );
+  }
+
+  function StaggerTextRightToLeft(char) {
+    CreateGsapTimeline({ trigger: char, markers: false }).fromTo(
+      new SplitType(char, { types: "chars" }).chars,
+      {
+        opacity: 0,
+        x: 800,
+      },
+      {
+        opacity: 1,
+        stagger: 0.5,
+        x: 5,
+      }
+    );
+  }
+
+  function ShapeRightToLeft(elem) {
+    CreateGsapTimeline({ trigger: elem.parentElement, markers: false }).fromTo(
+      `.${elem.className.split(" ")[0]}`,
+      {
+        opacity: 0,
+        rotate: 360,
+        scale: 0,
+        x: 800,
+      },
+      {
+        opacity: 1,
+        rotate: 0,
+        scale: 1,
+        x: 5,
+      }
+    );
+  }
+
+  function StaggerTextLeftToRight(char) {
+    CreateGsapTimeline({ trigger: char, markers: false }).fromTo(
+      new SplitType(char, { types: "chars" }).chars,
+      {
+        opacity: 0,
+        x: -800,
+      },
+      {
+        opacity: 1,
+        stagger: 0.5,
+        x: 5,
+      }
+    );
+  }
+
+  function StaggerTextFromY(char) {
+    CreateGsapTimeline({ trigger: char, markers: true }).fromTo(
+      new SplitType(char, { types: "chars" }).chars,
+      {
+        opacity: 0,
+        transformOrigin: "top",
+        y: -100,
+      },
+      {
+        opacity: 1,
+        stagger: 0.5,
+        y: 0,
+      }
+    );
+  }
+
+  function ShapeShowCenter(elem) {
+    CreateGsapTimeline({ trigger: elem.parentElement, end: "center 65%", markers: true }).fromTo(
+      `.${elem.className.split(" ")[0]}`,
+      {
+        opacity: 0,
+        rotate: 360,
+        scale: 0,
+      },
+      {
+        opacity: 1,
+        rotate: 0,
+        scale: 1,
+      }
+    );
+  }
 }
 
 export default Home;
